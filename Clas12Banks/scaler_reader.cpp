1366e959ff18756cbd8b3a54694a520793ff4fb1
dglazier <Derek.Glazier@glasgow.ac.uk>
2020-02-21 18:09:36 +0000

    add scaler_reader for finding accumulated beam charge : c12.getRunBeamCharge(); and event -by -event estimate c12.getCurrApproxCharge()

--- Clas12Banks/scaler_reader.cpp
+++ Clas12Banks/scaler_reader.cpp
@@ -12,6 +12,8 @@ namespace clas12{
     while(_reader.next()){
       _reader.read(_event);
       _event.getStructure(*_bscal.get());
+      if(_bscal->getRows()==0)continue; //only RUN::scaler
+
       auto cup=_bscal->getFCupGated();
       if(cup>_maxCup) _maxCup=cup;
       if(cup<_minCup) _minCup=cup;
