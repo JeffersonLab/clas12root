c5d6eff00082454b824ca37241b2c37970537c21
Adam Thornton <ademus4@gmail.com>
2020-09-15 17:16:57 +0100

    Start of ccdb reader work

---
+++ Clas12Banks/ccdb_reader.cpp
@@ -0,0 +1,5 @@
+#include "ccdb_reader.h"
+
+namespace clas12 {
+
+}
---
+++ Clas12Banks/ccdb_reader.h
@@ -0,0 +1,29 @@
+#ifndef CCDB_READER_H
+#define CCDB_READER_H
+
+#include "ccdb_vals.h"
+
+#include <iostream>
+
+#include <chrono>
+#include <string>
+
+namespace clas12 {
+  using std::cout;
+  using std::endl;
+  using std::string;
+
+  
+  class ccdb_reader {
+
+  public:
+
+    ccdb_reader();
+    virtual ~ccdb_reader();
+    
+  private:
+
+  };
+
+}
+#endif /* CCDB_READER_H */
