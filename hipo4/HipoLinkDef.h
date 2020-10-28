98b38eced9fb9cac5804dbea56ed517fa044f35c
dglazier <Derek.Glazier@glasgow.ac.uk>
2020-10-28 10:01:44 +0000

    add include <memory>

--- hipo4/HipoLinkDef.h
+++ hipo4/HipoLinkDef.h
@@ -12,7 +12,7 @@
 #pragma link C++ class hipo::bank+;
 #pragma link C++ class hipo::reader+;
 #pragma link C++ class hipo::writer+;
-#pragma link C++ class hipo::ntuple_writer+;
-#pragma link C++ class hipo::ntuple_reader+;
+//#pragma link C++ class hipo::ntuple_writer+;
+//#pragma link C++ class hipo::ntuple_reader+;
 #pragma link C++ class hipo::data_addrs_to_func+;
 #endif
