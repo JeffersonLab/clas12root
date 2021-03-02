3a6fe2540b68459e1626e0408405be3c9cc60931
Adam Thornton <ademus4@gmail.com>
2021-03-02 20:35:23 +0000

    Adding sample test data

--- tests/read_file.C
+++ tests/read_file.C
@@ -2,7 +2,7 @@
   //hipo file to be analysed
   //RGA pass 1, skim3, 5000 events
   HipoChain chain;
-  chain.Add("skim3_005036_subset.hipo");
+  chain.Add("./tests/data/EptopX2pi_sample.hipo");
 
   //check file exists
   int nfiles = chain.GetNFiles();
