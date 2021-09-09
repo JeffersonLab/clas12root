7b5a9a0b5c58fbed44166485810bf76cea379155
dglazier <Derek.Glazier@glasgow.ac.uk>
2021-09-09 08:49:02 +0100

    minor changes to examples

--- RunRoot/Ex4_TreeMaker.C
+++ RunRoot/Ex4_TreeMaker.C
@@ -5,14 +5,14 @@
   //treemaker.UseEventData();
   
   //make branch with given formula and alias it to name Time
-  //treemaker.Branch("P.Time-EVNT.StartTime/F","Time");
-  //treemaker.Branch("P.Time-EVNT.FTBStartTime/F","FTBTime");
-  //treemaker.Branch("P.Time-PBANK.Vt/F","FTBTime");
+  //give branch type with /F = float etc.
+  treemaker.Branch("P.Time-PBANK.Vt/F","ToF");
+  treemaker.Branch("P.Time-PBANK.FTBVt/F","FTBToF");
   treemaker.Branch("P.Time/F");
   treemaker.Branch("P.Path/F");
   treemaker.Branch("P.DetEnergy/P.P/F","SampFrac");
   treemaker.Branch("HTCC.Nphe/F");
-  //Create branch names as bank items
+
   treemaker.Branch("PBANK.Px/F");
   treemaker.Branch("PBANK.Py/F");
   treemaker.Branch("PBANK.Pz/F");
@@ -20,7 +20,7 @@
   treemaker.Branch("PBANK.Vy/F");
   treemaker.Branch("PBANK.Vz/F");
   treemaker.Branch("PBANK.Pid/I");
-  //treemaker.Branch("PBANK.FTBPid/I");//FT based PID
+  treemaker.Branch("PBANK.FTBPid/I");//FT based PID
 
   //Or for banks without links create one yourself
   //Useful for trajectories which require specific detector elements
@@ -31,7 +31,7 @@
   //treemaker.Branch("TRAJFTOFFTOF1A.Z/F");
 
   //e.g. Only save electron information
-  // treemaker.AddParticleCut("PBANK.Pid==11");
+  //treemaker.AddParticleCut("PBANK.Pid==11");
 
   //Event topology cuts
   //treemaker.AddAtLeastPid(211,1); //at least 1 pi+
