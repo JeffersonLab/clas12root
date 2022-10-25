28d9e8297f7f3c7ac5ca39c022ee2562b050f0d0
dglazier <Derek.Glazier@glasgow.ac.uk>
2022-10-25 17:39:39 +0100

    tidy up

--- RunRoot/Ex0_BankHist.C
+++ RunRoot/Ex0_BankHist.C
@@ -1,5 +1,5 @@
 {
-  BankHist bankDraw("/work/jlab/clas12data/dst_skim4_5038.hipo");
+  BankHist bankDraw("/where/is/my/file.hipo");
   bankDraw.Hist1D("REC::Event::StartTime",100,0,200,"")->Draw();
 
   bankDraw.Hist1D("REC::Particle::Pid",1000,-100,2500,"");
@@ -8,4 +8,8 @@
   bankDraw.Hist1D("REC::Particle::Pz",100,-5,5,"")->Draw("(2x2)");
 
 
+  bankDraw.Hist2D("REC::Particle::Px:REC::Particle::Py",100,-3,3,100,-3,3,"");
+  bankDraw.Hist1D("REC::Particle::Pid",1000,-100,2500,"");
+  bankDraw.Hist1D("sqrt(REC::Particle::Px*REC::Particle::Px+REC::Particle::Py*REC::Particle::Py+REC::Particle::Pz*REC::Particle::Pz)",200,0,10,"")->Draw("(3x1)colz");
+ 
 }
