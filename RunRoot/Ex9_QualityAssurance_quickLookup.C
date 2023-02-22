9200e578ce7c4281b34adb4464ad175210e52a95
dglazier <Derek.Glazier@glasgow.ac.uk>
2023-02-22 17:40:37 +0000

    rejig chargeAccumulation, should now work in chain and selector

--- RunRoot/Ex9_QualityAssurance_quickLookup.C
+++ RunRoot/Ex9_QualityAssurance_quickLookup.C
@@ -9,9 +9,9 @@ void Ex9_QualityAssurance_quickLookup(){
 
   /*Create a list of runs.
    6598, 6597 used as examples here.*/
-  std::vector<int> Runs;
-  Runs.push_back(6598);
-  Runs.push_back(6597);
+  std::set<int> Runs;
+  Runs.insert(6598);
+  Runs.insert(6597);
 
   /*Set up qadb_reader, can pass any run number (here 0), this isn't used*/
   qadb_reader qa(0);
@@ -21,5 +21,5 @@ void Ex9_QualityAssurance_quickLookup(){
 
   /*Ask for the accumulated charge in the list of runs.*/
   double charge=qa.getChargeForRunlist(Runs);
-  cout<<"Accumulated Charge "<<charge<<endl;
+  cout<<"Accumulated Charge "<<charge<<" nA"<<endl;
 }
